248e7ca3003b356082a365d2fc35b0955109993a
Bruce Zhou <zhou.ye@abc.net.au>
2019-07-31 09:38:34 +1000

    Check file exist before link coc-settings

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 sudo npm install -g dockerfile-language-server-nodejs
 sudo npm i -g bash-language-server
-ln coc-settings.json ~/.vim/coc-settings.json
+if [ ! -f ~/.vim/coc-settings.json ]; then
+	ln -s coc-settings.json ~/.vim/coc-settings.json
+fi
 vim +PlugInstall +"CocInstall coc-json coc-rls" +qall!
